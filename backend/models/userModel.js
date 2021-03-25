bfe542aa757d7d4ecd2e4528736cfb6e097746d9
OscarYemha <oscaryemha1990@gmail.com>
2021-03-25 01:42:10 -0300

    Create Sample Products

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, require: true },
+    name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, require: true },
-    isAdmin: { type: Boolean, default: false, require: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false, required: true },
   },
   {
     timestamps: true,
