d2215746a8b1cee2b47ea1dc3e4b4ec54dff0774
OscarYemha <oscaryemha1990@gmail.com>
2021-03-31 00:43:08 -0300

    Create Sign in Backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 
+dotenv.config();
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazonia", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -20,7 +25,7 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
